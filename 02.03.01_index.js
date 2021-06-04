14d8ac2f827a8f45171c6a823b445e7a5c2e687f
superluxubu <84695020+superluxubu@users.noreply.github.com>
2021-06-04 10:48:36 +0700

    Update 02.03.01_index.js

--- 02.03.01_index.js
+++ 02.03.01_index.js
@@ -15,28 +15,28 @@ const router = express.Router();
 //--- Home
 router.get('/',
     (req, res) => {
-        res.sendFile("./view/home.html");
+        res.sendFile(__dirname +"./view/home.html");
     }
 );
 
 //--- Profile
 router.get('/profile',
     (req, res) => {
-        res.sendFile("./view/profile.html");
+        res.sendFile(__dirname +"./view/profile.html");
     }
 );
 
 //--- Login
 router.get('/login',
     (req, res) => {
-        res.sendFile("./view/login.html");
+        res.sendFile(__dirname +"./view/login.html");
     }
 );
 
 //--- Logout
 router.get('/logout',
     (req, res) => {
-        res.sendFile("./view/logout.html");
+        res.sendFile(__dirname +"./view/logout.html");
     }
 );
 
